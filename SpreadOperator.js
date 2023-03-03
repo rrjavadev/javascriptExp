551961ecde0670eb7789a08adedb9be34ea51bb2
roshinivr <roshinirajesh1@gmail.com>
2023-03-03 20:43:30 +0000

    Update SpreadOperator.js
    Use Rest and Spread combined

--- SpreadOperator.js
+++ SpreadOperator.js
@@ -1,3 +1,6 @@
+/** 
+ * Rest and Spread together
+ * */
 const greet = function(...names) {
 	console.log('hello' + names.join(', '));
 };
@@ -10,3 +13,30 @@ greet(jack, jill);
 const nameArray = ['Tim', 'Monkey'];
 
 greet(...nameArray);
+
+/** 
+ * No Rest. Just spread
+ * */
+const names1 = ['A', 'B'];
+const names2 = ['Rock'];
+
+const sayHello = function(name1, name2){
+	console.log('hello' + name1 + ' and ' + name2);
+};
+
+sayHello(...names1);
+sayHello(...names2); //name2 will have no value and will be displayed as undefined.
+
+/** 
+ * Mixed parameters
+ * */
+
+const mixed = function(name1, name2, ...names){
+	console.log('name1: ' + name1);
+	console.log('name2: ' + name2);
+	console.log('names: ' + names.forEach(name => console.log(name)));
+}
+
+mixed('A',  ...['C','D','E']);
+
+
