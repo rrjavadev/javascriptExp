59f230a4465f14585adee567ebd25d7c1317e946
roshinivr <roshinirajesh1@gmail.com>
2023-03-06 22:20:50 +0000

    Add default parameter

---
+++ chapter1/defaultParameter-values.js
@@ -0,0 +1,33 @@
+
+const sortByTitle = function(books){
+  const byTitle = function(book1, book2){
+    return book1.title.localeCompare(book2.tile);
+  };
+  return books.slice().sort(byTitle);};
+
+
+const sortByTitle2 = function(books, ascending = true) { const multiplier = ascending ? 1 : -1;
+    const byTitle = function(book1, book2) {
+    return book1.title.localeCompare(book2.title) * multiplier;
+    };
+    return books.slice().sort(byTitle); };
+    
+  /**
+     * The reason for not using sort() directly is that it will 
+     * modify the array on which it is called—changing the input given to a 
+     * function is a poor programming practice. 
+     * The slice() function makes a copy of the given array and the sort() function then sorts the copy,
+     * thus not affecting the original array given as input.
+     */
+
+const books = [
+  {title: 'Who Moved My Cheese'},
+  {title: 'Great Expectations'},
+  {title: 'The Power of Positive Thinking'}
+];
+
+//Ascending
+console.log(sortByTitle(books));
+
+//Descending
+console.log(sortByTitle2(books, false));
