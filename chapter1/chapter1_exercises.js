bea445334db42dff610838be3e37d9a045383ed8
roshinivr <roshinirajesh1@gmail.com>
2023-03-12 23:06:47 +0000

    Add execise 2

--- chapter1/chapter1_exercises.js
+++ chapter1/chapter1_exercises.js
@@ -26,3 +26,21 @@ console.log(amountAfterTaxes(amount)); //25.12
 console.log(amountAfterTaxes(amount, fedTax)); //27.63
 console.log(amountAfterTaxes(amount, fedTax, stateTax)); //28.13
 console.log(amountAfterTaxes(amount, fedTax, stateTax, localTax)); //28.26
+
+/**
+ * Exercise 2: 
+ * The purchaseItems() function merely prints the parameters it receives, after a little formatting. Two calls to the function are shown. Let’s implement a third call to the function so that it produces the desired result.
+ */
+const purchaseItems = function(essential1, essential2, ...optionals) {
+  console.log(essential1 + ', ' + essential2 + ', ' + optionals.join(', '));
+};
+
+
+purchaseItems('bread', 'milk');
+purchaseItems('bread', 'milk', 'jelly');
+
+const mustHaves = ['bread', 'milk'];
+const andAlso = ['eggs', 'donuts', 'tea'];
+
+//call purchaseItems so it prints bread, milk, eggs, donuts, tea
+purchaseItems(...mustHaves, ...andAlso);
