a290475860ac921145cec3f45d463707c108549c
roshinivr <roshinirajesh1@gmail.com>
2023-03-13 08:39:36 +0000

    enhanced forloop

--- Chapter4_iterators_and_symbols/Enhanced_for.js
+++ Chapter4_iterators_and_symbols/Enhanced_for.js
@@ -4,5 +4,26 @@
 
 const names = ['Sara', 'Jake', 'Pete', 'Mark', 'Jill'];
 for(let i=0; i< names.length; i++){
-    console.log(names[i]);
+  console.log(names[i]);
 }
+
+/**
+ * Enhanced for-loop
+ */
+for(const name of names){//constant name has a block scope
+  console.log(name);
+}
+
+/**
+ * Enhanced for loop to obtain index
+ */
+
+for(const entry of names.entries()){
+  console.log(entry);
+}
+
+//destructure entries. Entries returns an iterator
+for(const [i, name] of names.entries()){
+  console.log('Index: ' + i + ' Name: ' + name);
+}
+
