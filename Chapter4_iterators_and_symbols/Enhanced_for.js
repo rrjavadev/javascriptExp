bce89eff33477147dbc4d4a040fc77cef7d984d1
roshinivr <roshinirajesh1@gmail.com>
2023-03-13 09:05:27 +0000

    Add Symbol property

--- Chapter4_iterators_and_symbols/Enhanced_for.js
+++ Chapter4_iterators_and_symbols/Enhanced_for.js
@@ -27,3 +27,39 @@ for(const [i, name] of names.entries()){
   console.log('Index: ' + i + ' Name: ' + name);
 }
 
+/**
+ * Symbol - A new primitive type
+ */
+
+const age = Symbol('ageValue');
+const email = 'emailValue';
+
+const sam = {
+  first: 'Sam',
+  [email]: 'sam@example.com',
+  [age]: 2
+};
+
+console.log('Iterating over properties using for .. in');
+for(const property in sam){
+  console.log('Property: ' + property); //Age won't be displayed
+}
+
+console.log('List of property names: ');
+console.log(Object.getOwnPropertyNames(sam)); //Symbol property hidden
+
+
+console.log('List of Symbol property names: ');
+console.log(Object.getOwnPropertySymbols(sam)); //Symbol property visible
+
+console.log('Accessing Symbol property: ');
+console.log(sam[age]); //Output 2
+
+
+console.log('Changing value of Symbol property: ');
+sam[age] = 3;
+console.log(sam[age]); //Symbol property changed to 3
+
+
+
+
