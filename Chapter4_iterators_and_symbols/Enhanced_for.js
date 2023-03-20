d9c2f0a27125899d9c0cb9900dcf5de96f2fb883
roshinivr <roshinirajesh1@gmail.com>
2023-03-20 07:34:51 +0000

    Global Registry Symbols

--- Chapter4_iterators_and_symbols/Enhanced_for.js
+++ Chapter4_iterators_and_symbols/Enhanced_for.js
@@ -65,3 +65,32 @@ console.log(sam[age]); //Symbol property changed to 3
  * Built-in Objects for Symbols
  */
 
+const regex = /cool/;
+
+process.stdout.write('regex is of type RegExp: ');
+console.log(regex instanceof RegExp);
+
+process.stdout.write('Properties of regex: ');
+console.log(Object.getOwnPropertyNames(regex));
+
+process.stdout.write('Symbol properties of regex: '); 
+console.log(Object.getOwnPropertySymbols(regex));
+
+console.log("Symbol properties of regex's prototype: "); 
+console.log(Object.getOwnPropertySymbols(Object.getPrototypeOf(regex)));
+
+/**
+ * Global Registry Symbol
+ */
+
+const name1 = 'Tom';
+const tom = Symbol(name1);
+
+const jerry = Symbol('Jerry');
+const anotherTom = Symbol(name1);
+
+console.log(tom);
+console.log('Type of Tom: ' + typeof(tom));
+console.log('Are Tom and Jerry the same? ' + (tom === jerry));
+console.log('Is Tom == anotherTom? ' + (tom === anotherTom));
+
